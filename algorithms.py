a150318d3c0c403124147edeb08752d4d2294ebb
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-11 20:30:20 -0400

    Updated with Task 1

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,24 @@
 #Task 1
+
+#Checks if the inputted string is an int or a float
+def isintorfloat(num):
+    try:
+        float(num)
+        return True
+    except:
+        return False
+
 num = input("How many hours do you want to convert?\n")
-print(num + " hours is equivalent to " + num*60 + " minutes.")
+
+#Catches when the user accidentally inputs a string
+while isintorfloat(num) != True:
+    num = input("Sorry, that is not a number. Input again here:\n")
+    if isintorfloat(num):
+        break
+
+newnum = float(num)
+newnum *= 60
+
+print(str(num) + " hours is equivalent to " + str(newnum) + " minutes.")
+
+#Task 2
