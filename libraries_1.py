519d7e6d419b7623e0ab27c7360937f605939a1c
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-13 16:36:12 -0400

    changed libraries_1

--- libraries_1.py
+++ libraries_1.py
@@ -4,20 +4,20 @@ from random import shuffle
 from random import randint
 from random import choice
 
-result_1 = math.pow(2,4)
+result_1 = pow(2,4)
 print("result 1 is", result_1)
 
-result_2 = math.sqrt(16)
+result_2 = sqrt(16)
 print("result 2 is", result_2)
 
-result_3 = random.randint(0,100)
+result_3 = randint(0,100)
 print("result 3 is", result_3)
 
 names = ["Amaryllis", "Godson", "Emily", "Reina", "Derin", "Elena", "Inacio"]
 print("Original names", names)
 
-random.shuffle(names)
+shuffle(names)
 print("Names after shuffling:", names)
 
-result_4 = random.choice(names)
+result_4 = choice(names)
 print("Chosen name is:", result_4)
