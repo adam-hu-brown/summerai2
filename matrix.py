f8de3b5c640c2d5a7b3323b617f05b8fca464b04
Adam Hu <adamhu@Adams-MacBook-Air-2.local>
2023-07-14 15:58:27 -0400

    renamed to matrix.py

---
+++ matrix.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+A = [[3,5],[2,4]]
+B = [[1,2],[3,4]]
+C = np.add(A,B)
+print("Element-wise sum of A and B is: ")
+print(C)
+
+D = np.subtract(A,B)
+print("\nElement-wise subtraction of A and B is: ")
+print(D)
+
+E = np.multiply(A,B)
+print("\nElement-wise multiplication of A and B is: ")
+print(E)
+
+F = np.dot(A,B)
+print("\nDot product of A and B is: ")
+print(F)
